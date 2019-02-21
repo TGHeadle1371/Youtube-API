47a6ee3047640cac085b682580c64218a4b7ce82
TGHeadle1371 <tgheadle@gmail.com>
2019-02-21 16:18:39 -0700

    Channel api js

--- main.js
+++ main.js
@@ -66,8 +66,47 @@ function handleSignoutClick() {
     gapi.auth2.getAuthInstance().signOut();
 }
 
+// Display Channel data
+function showChannelData(data) {
+    var channelData = document.getElementById("channel-data");
+    channelData.innerHTML = data;
+}
+
 // Get channel from API
 
 function getChannel(channel) {
-    console.log(channel);
+    gapi.client.youtube.channels
+        .list({
+            part: "snippet,contentDetails,statistics",
+            forUsername: channel
+        })
+        .then(response => {
+            console.log(response);
+            var channel = response.result.items[0];
+
+            var output = `
+                <ul class="collection">
+                <li class="collection-item"> Title: ${
+                    channel.snippet.title
+                } </li>
+                <li class="collection-item"> ID: ${channel.id} </li>
+                <li class="collection-item"> Subscribers: ${
+                    channel.statistics.subscriberCount
+                } </li>
+                <li class="collection-item">Views: ${
+                    channel.statistics.viewCount
+                }</li>
+                <li class="collection-item">Videos: ${
+                    channel.statistics.videoCount
+                }</li>
+                </ul>
+                <p>${channel.snippet.description}</p>
+                <hr>
+                <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${
+                    channel.snippet.customUrl
+                }"> Visit Channel </a>
+            `;
+            showChannelData(output);
+        })
+        .catch(err => alert("No Channel By That Name"));
 }
