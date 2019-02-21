5fb9c613ccb49a8ea83d7629efc95633bdb9f76a
TGHeadle1371 <tgheadle@gmail.com>
2019-02-21 15:50:29 -0700

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+// Options
+var CLIENT_ID = '86054638228-5ub3b4elfct0pg4bouua516va49g7eht.apps.googleusercontent.com';
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+// Authorization scopes required by the API. If using multiple scopes,
+// separated them with spaces.
+var SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+var authorizeButton = document.getElementById('authorize-button');
+var signoutButton = document.getElementById('signout-button');
